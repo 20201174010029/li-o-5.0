23eadaf7123e946a04c78f3bd4c417e4d4cacdd4
JOAO DEYBSON <99667175+20201174010029@users.noreply.github.com>
2022-02-24 14:04:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+j= float(input('digite o primeiro que venho na sua mente !  '))
+a= float(input('digite o segundo numero por favor! '))
+s = j / a
+print('esse foi o resultado esse numero sera especial para voce cuide dele pfvr!' ,s)
+
+print('obrigado por esta aqui!')
